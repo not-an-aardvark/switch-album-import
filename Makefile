0000000dc7510686ce83191cfc5d9681c12d1067
Teddy Katz <teddy.katz@gmail.com>
2020-12-01 04:14:06 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+INSTALL ?= install
+prefix ?= /usr/local
+bindir ?= $(prefix)/bin
+
+all: bin/switch-album-import
+
+bin/switch-album-import: $(wildcard *.swift)
+	@mkdir -p $(@D)
+	xcrun -sdk macosx swiftc -target x86_64-apple-macosx10.10 $+ -O -o $@
+
+install: bin/switch-album-import
+	$(INSTALL) -b -v $< $(DESTDIR)$(bindir)
+
+uninstall:
+	rm -f $(DESTDIR)$(bindir)/switch-album-import
+
+link: bin/switch-album-import
+	stow -vt $(DESTDIR)$(bindir) bin
+
+clean:
+	rm -f bin/switch-album-import
+
+test:
+	swiftformat --lint *.swift
+	swiftlint lint *.swift
---
+++ main.swift
@@ -0,0 +1,108 @@
+import CoreWLAN
+import Foundation
+
+private func printUsage() {
+    print("""
+    Usage: switch-album-import -h|-help|--help
+           switch-album-import -ssid <ssid> -password <password> -output_dir <dir>
+    """)
+}
+
+func main() {
+    let args = Array(CommandLine.arguments.dropFirst())
+    if (args.contains { arg in arg == "-h" || arg == "-help" || arg == "--help" }) {
+        printUsage()
+        exit(0)
+    }
+
+    let defaults = UserDefaults.standard
+
+    guard let ssid = defaults.string(forKey: "ssid"),
+          let password = defaults.string(forKey: "password"),
+          let output_dir = defaults.string(forKey: "output_dir")
+    else {
+        printUsage()
+        exit(1)
+    }
+
+    var output_dir_is_directory: ObjCBool = true
+    if !FileManager.default.fileExists(atPath: output_dir, isDirectory: &output_dir_is_directory) {
+        print("[ERROR] No such directory: \(output_dir)")
+        exit(1)
+    }
+
+    print("[INFO] Connecting to \(ssid)...")
+
+    let client = CWWiFiClient.shared()
+
+    guard let interface = client.interface(), let switch_hotspot = (try? interface.scanForNetworks(withName: ssid))?.first else {
+        print("[ERROR] Failed to find switch hotspot at \(ssid)")
+        exit(1)
+    }
+
+    do {
+        try interface.associate(to: switch_hotspot, password: password)
+    } catch {
+        print("[ERROR] Failed to connect to switch hotspot at \(ssid)")
+        interface.disassociate()
+        exit(1)
+    }
+    print("[INFO] Successfully connected to \(ssid)")
+
+    guard let index_contents = try? Data(contentsOf: URL(string: "http://192.168.0.1/data.json")!),
+          let parsed_index = try? JSONSerialization.jsonObject(with: index_contents),
+          let parsed_dict = parsed_index as? [String: Any],
+          let console_name = parsed_dict["ConsoleName"] as? String,
+          let filenames = parsed_dict["FileNames"] as? [String]
+    else {
+        print("[ERROR] Failed to download index file from switch")
+        interface.disassociate()
+        exit(1)
+    }
+
+    print("[INFO] Downloading \(filenames.count) file(s) from \(console_name)...")
+    let semaphore = DispatchSemaphore(value: 0)
+    for filename in filenames {
+        // Sanity check to ensure filenames are reasonable and don't contain e.g. path components
+        if filename.range(of: #"^\w[\w.-]+$"#, options: .regularExpression) == nil {
+            print("Unexpected filename: \(filename)")
+            interface.disassociate()
+            exit(1)
+        }
+
+        let url = URL(string: "http://192.168.0.1/img/\(filename)")!
+        print("[INFO] Downloading \(filename)...")
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+
+            guard let data = data else {
+                print("[ERROR] Failed to download \(url): \(String(describing: error))")
+                interface.disassociate()
+                exit(1)
+            }
+            let file_url = URL(fileURLWithPath: output_dir).appendingPathComponent(filename)
+            do {
+                try data.write(to: file_url)
+            } catch {
+                print("[ERROR] Failed to write to \(file_url)")
+                interface.disassociate()
+                exit(1)
+            }
+            semaphore.signal()
+        }
+        task.resume()
+    }
+    for _ in filenames {
+        semaphore.wait()
+    }
+    print("[INFO] Successfully downloaded \(filenames.count) file(s) from \(console_name)")
+
+    // Disconnect from the Switch hotspot.
+    // It doesn't seem to be possible to reconnect back to the previous network here. However,
+    // the OS should do it automatically since it won't be connected to any networks at this
+    // point. Note that the OS might try to reconnect to the Switch hotspot instead; to prevent
+    // this, the Switch's SSID should be configured to *not* auto-join in System Preferences.
+    interface.disassociate()
+    print("[INFO] Disconnected from \(ssid)")
+}
+
+main()
